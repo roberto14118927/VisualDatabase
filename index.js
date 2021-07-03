88f2c24ef2ab4e889a3358beae0a83baae367a47
Roberto Guzman <robertoguzman@MacBook-Pro-de-Roberto-2.local>
2021-07-03 09:51:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+var nameData = '';
+var lastName = '';
+var statusData = true;
+var longitud = 5;
+var conta = 0;
+var rHtml = "";
+// Recivir el dato de HTML
+// function sendData() {
+//     nameData = document.getElementById('name').value
+//     lastName = document.getElementById('lastName').value
+
+//     if (nameData == '' && lastName == '') {
+//         alert("llene los campos")
+//     }
+//     else {
+//         alert(nameData + " " + " " + lastName);
+//     }
+
+
+// }
+
+// Ocultar los elementos por el Id asignado a la vista en HTML
+// function sendData() {
+
+//     statusData = !statusData;
+//     console.log(statusData);
+
+//     if (statusData == false) {
+//         document.getElementById('name').style.display = 'block';
+//         document.getElementById('lastName').style.display = 'block';
+//     }
+//     else {
+//         document.getElementById('name').style.display = 'none';
+//         document.getElementById('lastName').style.display = 'none';
+//     }
+
+// }
+
+// Rendereizar vista en top con interpola ion en javascript
+// function sendData() {
+//     for (i = 2; i <= longitud; i++) {
+//         document.getElementById('id' + i + '').style.top = '' + conta + 'px';
+//         conta = conta + 15;
+//     }
+
+// }
+
+// contruir vista de html desde javascript
+// function sendData() {
+//     for (i = 1; i <= longitud; i++) {
+//         rHtml += "<input class='input-custom input is-primary' id='id" + i + "'  type='text' placeholder='Primary input' style='top: " + conta + "px'> ";
+//         conta = conta + 15;
+//     }
+//     document.getElementById('renderHtml').innerHTML = rHtml;
+//     document.getElementById('boton1').style.top = '' + conta + 'px';
+
+// }
+
